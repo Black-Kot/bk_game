e2ca711d93bfee7136bcb1cc0c50b9522179cb2a
Black_Kot <asakura1313@ya.ru>
2014-05-19 17:27:36 +0400

    fix height in ore generator#3

--- mods/ores/init.lua
+++ mods/ores/init.lua
@@ -168,6 +168,12 @@ local function generate_ore(name, wherein, minp, maxp, seed, chunks_per_volume, 
 	if y_min < y_max-chunk_size then
 		y_min = y_max-(chunk_size*2)
 	end
+	if minp.x < maxp.x-chunk_size then
+		minp.x = maxp.x-(chunk_size*2)
+	end
+	if minp.z < maxp.z-chunk_size then
+		minp.z = maxp.z-(chunk_size*2)
+	end
 	local volume = (maxp.x-minp.x+1)*(y_max-y_min+1)*(maxp.z-minp.z+1)
 	local pr = PseudoRandom(seed)
 	local num_chunks = math.floor(chunks_per_volume * volume)
