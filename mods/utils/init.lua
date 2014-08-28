4574c0acf67890ce847f5e8cd3ec5b604a5efa4a
Black_Kot <asakura1313@ya.ru>
2014-08-28 05:11:25 +0400

    change stack_max to 100 globally

--- mods/utils/init.lua
+++ mods/utils/init.lua
@@ -1,3 +1,8 @@
 bk_game = {}
 
 dofile(minetest.get_modpath("utils").."/helper_functions.lua")
+
+-- change max_stack globally
+core.nodedef_default.stack_max = 100
+core.craftitemdef_default.stack_max = 100
+core.noneitemdef_default.stack_max = 100
